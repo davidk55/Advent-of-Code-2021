abfd1b95cec4e2ef3a53f4b2988b67e3079a114b
David <david.programming@protonmail.com>
2021-12-05 18:22:47 +0100

    Answer for Day 5 Part 1

---
+++ day5/src/Diagram.java
@@ -0,0 +1,47 @@
+public class Diagram {
+    private final int[][] diagram;
+
+    public Diagram(int MAX_LINES) {
+        diagram = new int[MAX_LINES][MAX_LINES];
+    }
+
+    public void addLine(int x1, int y1, int x2, int y2) {
+        int cache;
+        if (x1 > x2) {
+            cache = x1;
+            x1 = x2;
+            x2 = cache;
+        }
+        if (y1 > y2) {
+            cache = y1;
+            y1 = y2;
+            y2 = cache;
+        }
+        if (x1 == x2 || y1 == y2) {
+            for (int i = x1; i <= x2; i++) {
+                for (int j = y1; j <= y2; j++) {
+                    diagram[i][j]++;
+                }
+            }
+        }
+    }
+
+    public int countPositionsTwoOrMore() {
+        int counterTwoOrMore = 0;
+        for (int[] i : diagram) {
+            for (int j : i) {
+                if (j >= 2) counterTwoOrMore++;
+            }
+        }
+        return counterTwoOrMore;
+    }
+
+    public int[] lineSegmentToLineCoordinates(String line) {
+        String[] s = line.trim().split(",|\\s->\\s");
+        int[] lineCoordinates = new int[4];
+        for (int i = 0; i < 4; i++) {
+            lineCoordinates[i] = Integer.parseInt(s[i]);
+        }
+        return lineCoordinates;
+    }
+}
